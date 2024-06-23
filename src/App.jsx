669c16b6df47eb36952d3319ba92d8400fbec754
harshrao26 <harshurao058@gmail.com>
2024-06-23 12:36:33 +0530

    TopBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Button from "./components/Button";
 import TopNav from "./components/TopNav";
 import SideNav from "./components/SideNav";
+import TopBar from "./components/TopBar";
 function App() {
   return (
     <>
-      <div className="relative">
-        <div className="">
+      <div className="relative bg-[#0D0D0D]">
+        <div className="flex">
           <SideNav className="" />
+        <TopBar />
 
         </div>
         <div>
